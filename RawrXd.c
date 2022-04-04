f21c12c5dc3928dc11c03b646f41655f98794105
GRun03 <gavin@therunnerhouse.com>
2022-04-03 21:03:24 -0400

    first commit

---
+++ RawrXd.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+    int num1;
+    printf("enter a numb and ill print it: \n");
+    scanf("%d", &num1);
+    printf("%d", num1);
+    return 0;
+}
